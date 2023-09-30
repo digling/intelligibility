1c5f16825369f230d4881f98956cad43503017cf
lingulist <mattis.list@lingulist.de>
2023-09-30 19:55:28 +0200

    update

---
+++ raw/samplesize.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/SequenceComparison/listsamplesize.git
+edictor wordlist --name=samplesize --dataset=listsamplesize/cldf/cldf-metadata.json --addon="cogid_cognateset_id:cogid"
